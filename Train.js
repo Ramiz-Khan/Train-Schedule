336fd2001425c691c17244ad0b2e81f46ecdd4f7
Ramiz khan <ramizkhan@Ramizs-MacBook-Pro-2.local>
2017-09-22 16:54:09 -0500

    fixed Page issue

--- Train.js
+++ Train.js
@@ -1,6 +1,25 @@
 
-
 $(document).ready( function () {
+
+
+	//var db = firebase.database();
+
+		
+	//	db.ref("Train1").set("/Fast-Train/stats1");
+	//	db.ref("/Fast-Train/stats1").set({"Name" : "Fast Train", "Destination" : "Dallas", "Frequency" : "35", "Next Arrival" : "12:53", "Minutes Away" : "35"});
+
+		//train 2
+		
+	//	db.ref("Train2").removeValue("/Slow-train/stats2");
+	//	db.ref("/Slow-train/stats2").set({"Name" : "Second Train", "Destination" : "Austin", "Frequency" : "56", "Next Arrival" : "2:53", "Minutes Away" : "85"});
+
+
+
+	//	db.ref().on("value" , (function(dbvalue){
+
+	//	}));
+
+
 
 $("div#button.btn.btn-info").on("click", (function () {
 	alert("What up bro. Everything is working good. ");
